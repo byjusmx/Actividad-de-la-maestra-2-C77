5f5f9bd8949fb6efe03f5552ec29d2ae5e6c9219
byjusmx <87218747+byjusmx@users.noreply.github.com>
2021-10-18 21:20:27 -0500

    Update IssLocation.js

--- screens/IssLocation.js
+++ screens/IssLocation.js
@@ -10,7 +10,7 @@ export default class IssLocationScreen extends Component {
                     justifyContent: "center",
                     alignItems: "center"
                 }}>
-                <Text>ISS Location Screen!</Text>
+                <Text>Pantalla localización EEI!</Text>
             </View>
         )
     }
